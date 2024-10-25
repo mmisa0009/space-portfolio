92f8769e4bcce6c39c0b2f87f315905a31fa1c29
MISAKI <misaki@Shinnosuke-no-MacBook-Pro.local>
2024-10-25 13:26:25 +0200

    Initial commit from Create Next App

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
-        <StarsCanvas />
         <Navbar />
         {children}
         <Footer />
